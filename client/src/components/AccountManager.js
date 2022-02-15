09cf463ff82ae9607947619270fc27a028a1b13b
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-02-15 13:41:15 +0000

    fix #10

--- client/src/components/AccountManager.js
+++ client/src/components/AccountManager.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 import AccountSummary from "./AccountSummary";
@@ -14,6 +14,10 @@ function AccountManager(props) {
     let initialState;
     props.isAuthenticated ? initialState = "Summary" : initialState = "Login";
     let [managerState, setManagerState] = useState(initialState);
+
+    useEffect(() => {
+        props.isAuthenticated ? setManagerState("Summary") : setManagerState("Login");
+    }, [props.isAuthenticated])
 
     return(
         <div id="accounts-wrap">
