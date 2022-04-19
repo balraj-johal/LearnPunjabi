14655d5bf6a249ed7bee07860c9695121bf3b1e7
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-04-19 13:05:42 +0100

    add placeholder content

--- client/src/components/Welcome/ThreeThings.js
+++ client/src/components/Welcome/ThreeThings.js
@@ -7,18 +7,18 @@ function ThreeThings(props) {
         <div id="three-things">
             <Thing 
                 index={1} 
-                title={"title"} 
-                text="Learn Punjabi quickly and effectively using our intuitive learning system." 
+                title="Learn Quickly"
+                text="Only have 5 minutes? No worries, our platform is designed to teach you in short bursts of content." 
             /> 
             <Thing 
                 index={2} 
-                title={"title"} 
-                text="Learn Punjabi quickly and effectively using our intuitive learning system." 
+                title="Learn Effectively"
+                text="Using proven learning techniques such as Spaced Repetition and the Heisig technique we make it easy to learn a new alphabet!" 
             /> 
             <Thing 
                 index={3} 
-                title={"title"} 
-                text="Learn Punjabi quickly and effectively using our intuitive learning system." 
+                title="Learn Enjoyably"
+                text="Our lessons are designed to be quick and enjoyable to complete, because no one learns when they're bored or frustrated!" 
             /> 
         </div>
     )
