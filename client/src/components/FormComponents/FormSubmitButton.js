0aefb37e60aee6b6fc4d57258b2a1c2ba74b2368
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-04-26 16:10:57 +0100

    update button colour

--- client/src/components/FormComponents/FormSubmitButton.js
+++ client/src/components/FormComponents/FormSubmitButton.js
@@ -5,7 +5,7 @@ function FormSubmitButton(props) {
         <div className={`button-wrap-${props.for} my-8`} >
             <button
                 type="submit"
-                className="capitalize h-10 bg-blue-500 rounded text-white px-4"
+                className="capitalize h-10 bg-primary rounded text-white px-4"
             >
                 {props.text || props.for}
             </button>
