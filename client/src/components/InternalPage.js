7e5aafd724aff2167328e2cfe892e1fa89745fc1
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-01 00:21:04 +0100

    programatically set container height

--- client/src/components/InternalPage.js
+++ client/src/components/InternalPage.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {  } from "react";
 import Topbar from "./Topbar";
 
 function InternalPage({ children, ...props }) {
+    
+    /** gets the height in pixels of the topbar-wrap, including if present, the leaderboard
+     * @name getTopbarHeight
+     * @returns {Number} height
+     */
+    let getTopbarHeight = () => {
+        let topbarWrap = document.getElementById("topbar-wrap");
+        if (!topbarWrap) return;
+        console.log(topbarWrap.clientHeight)
+        return topbarWrap.clientHeight;
+    }
+    
     return(
         <>
             <Topbar />
-            <div className="container">
+            <div className="container flex" style={{height: `calc(100vh - ${getTopbarHeight()}px)`}} >
                 { children }
             </div>
         </>
