970571983b5e48e5477994291f067c083affe550
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-02 00:23:16 +0100

    remove tailwind class causing build errors

--- client/src/components/CourseComponents/ProgressBar.js
+++ client/src/components/CourseComponents/ProgressBar.js
@@ -6,7 +6,7 @@ function ProgressBar(props) {
         <div className="w-full flex justify-center absolute top-10">
             <div className="h-2 w-10/12 bg-gray-400 rounded-xl left-1/12">
                 {/* for some reason the tailwind width class is not applying correctly.   */}
-                <div className={`rounded-xl h-full w-[${props.percent}%] bg-primary transition-all duration-300`} 
+                <div className={`rounded-xl h-full bg-primary transition-all duration-300`} 
                     style={{width: `${props.percent}%`}}>
                 </div>
             </div>
