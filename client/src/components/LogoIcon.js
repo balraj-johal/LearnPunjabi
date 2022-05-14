6357ed04b7dde57faab2b908d3c4ad750189ed77
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-14 14:53:34 +0100

    shrink icon logo slightly

--- client/src/components/LogoIcon.js
+++ client/src/components/LogoIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function LogoIcon(props) {
     return(
-        <svg className="no-highlight" id="logo-icon" width="46.15" height="50" viewBox="0 0 36 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="no-highlight" id="logo-icon" width="44.3" height="50" viewBox="0 0 36 39" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 8C18 8 18 19 35.5 19M0 2C18 2 18 13 35.5 13M0 14C18 14 18 25 35.5 25M0 20C18 20 18 31 35.5 31M0 26C18 26 18 37 35.5 37" stroke="#00A3FF" strokeWidth="3"/>
         </svg>
 
