9fb95ca36fc009e9210f7ee839bb8a51ef0510e5
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-16 16:23:20 +0100

    add active style to account tab

--- client/src/components/AccountManagement/AccountTab.js
+++ client/src/components/AccountManagement/AccountTab.js
@@ -6,6 +6,7 @@ function AccountTab(props) {
             className={`text-lg w-6/12 h-10 md:h-12 transition-all no-highlight pt-[3px]
                 ${props.first ? "border-r-2" : ""}  border-black 
                 flex justify-center items-center cursor-pointer border-b-2
+                active:bg-primary2 active:text-white
                 ${props.managerState === props.for ? 
                     "bg-primary text-white" : 
                     "hover:bg-slate-200"
