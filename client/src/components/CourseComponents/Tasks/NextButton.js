16c86d27b6b2c39c0ccde9ec32ce36909628e901
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-16 23:46:58 +0100

    change size of next task button

--- client/src/components/CourseComponents/Tasks/NextButton.js
+++ client/src/components/CourseComponents/Tasks/NextButton.js
@@ -2,8 +2,10 @@ import React, {  } from "react";
 
 function NextButton(props) {
     return(
-        <button className="md:w-28 w-3/6 text-white h-8 bg-primary cursor-pointer rounded absolute bottom-0 right-0
-            hover:bg-primary2 mb-4 mt-6 hover:drop-shadow-md mr-4 capitalize transition-all duration-75" 
+        <button className="md:w-36 w-3/6 text-white h-8 bg-primary 
+            cursor-pointer rounded absolute bottom-0 right-0
+            hover:bg-primary2 mb-4 mt-6 hover:drop-shadow-md mr-4 
+            capitalize transition-all duration-75" 
             onClick={()=>{ props.next() }}
         >
             {props.text || "Next"} &gt;
