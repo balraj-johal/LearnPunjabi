43cb654f488401d1e4462de0a250e21aae8b0f84
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-25 20:40:49 +0100

    widen next button

--- client/src/components/CourseComponents/Tasks/NextButton.js
+++ client/src/components/CourseComponents/Tasks/NextButton.js
@@ -2,10 +2,11 @@ import React, {  } from "react";
 
 function NextButton(props) {
     return(
-        <button className="md:w-36 w-3/6 text-white h-8 bg-primary 
-            cursor-pointer rounded absolute bottom-0 right-0
-            hover:bg-primary2 mb-6 md:mb-4 mt-6 hover:drop-shadow-md mr-4 
-            capitalize transition-all duration-75" 
+        <button 
+            className="md:w-44 w-3/6 text-white h-8 bg-primary 
+                cursor-pointer rounded absolute bottom-0 right-0
+                hover:bg-primary2 mb-6 md:mb-4 mt-6 hover:drop-shadow-md 
+                mr-4 capitalize transition-all duration-75" 
             onClick={()=>{ props.next() }}
         >
             {props.text || "Next"} &gt;
