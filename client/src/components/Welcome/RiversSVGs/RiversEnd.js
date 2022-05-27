d675172235d4a3290312a402188fb4b4ce7444dc
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-27 17:14:15 +0100

    handle #105 for larger view heights

--- client/src/components/Welcome/RiversSVGs/RiversEnd.js
+++ client/src/components/Welcome/RiversSVGs/RiversEnd.js
@@ -26,8 +26,23 @@ function RiversEnd(props) {
             { mapArray.map((elem, index) => (
                 <RiversStraight index={index+1} height={svgHeight} key={index} end="end" />
             )) }
-            <svg id="rivers-end" ref={bottom} width="30%" viewBox="0 0 558 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path height="100%"  d="M113.143 1000C113.143 444.282 298.571 444.282 298.571 -96M12 1000C12 444.282 197.429 444.282 197.429 -96M214.286 1000C214.286 444.282 399.714 444.282 399.714 -96M315.429 1000C315.429 444.282 500.857 444.282 500.857 -96M416.571 1000C416.571 444.282 602 444.282 602 -96" stroke="#00A3FF" strokeWidth="23"/>
+            <svg 
+                id="rivers-end" 
+                ref={bottom} 
+                width="30%" 
+                viewBox="0 0 558 1000" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path 
+                    height="100%"
+                    stroke="#00A3FF" 
+                    strokeWidth="23"
+                    d="M113.143 1000C113.143 444.282 298.571 444.282 298.571 -96M12 1000C12
+                        444.282 197.429 444.282 197.429 -96M214.286 1000C214.286 444.282 
+                        399.714 444.282 399.714 -96M315.429 1000C315.429 444.282 500.857
+                        444.282 500.857 -96M416.571 1000C416.571 444.282 602 444.282 602 -96" 
+                />
             </svg>
         </>
     )
