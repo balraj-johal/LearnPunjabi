51d633e2d82fcc3af6bc844c444f87ee1cf7d71e
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-05-28 15:06:52 +0100

    close #170

--- client/src/components/CourseComponents/LessonIcon.js
+++ client/src/components/CourseComponents/LessonIcon.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Lottie from "react-lottie-player";
 
 import { randNo } from "../../utils/math";
@@ -21,27 +21,31 @@ function LessonIcon(props) {
     }, [props.timesCompleted])
 
     return(
-        <div 
-            className={`${finished ? "bg-[#FFD700]" : "bg-white"} 
-                lesson z-20 relative overflow-hidden no-highlight`}
-            onClick={() => { navigate(`/lesson/${props.lesson.id}`) }}
-        >
-            <span className="z-10">{ props.lesson.name }<br/></span>
-            <div 
-                className={`${finished ? "" : "bg-primary"} 
-                    z-0 w-full absolute bottom-0`} 
-                style={{height: `${ waterFillAmount > 150 ? "100" : waterFillAmount }%`}} 
-            />
-            { showWave ? <Lottie 
-                rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }} 
-                className={`w-full h-full absolute ${ANIMATION_OFFSET_CLASS}`}
-                style={{ transform: `translate(0, -${waterFillAmount}px)` }}
-                animationData={wave} 
-                goTo={startFrame} 
-                loop 
-                play 
-            /> : null }
-        </div>
+        <Link to={`/lesson/${props.lesson.id}`}>
+            <div className={`${finished ? "bg-[#FFD700]" : "bg-white"} 
+                    lesson z-20 relative overflow-hidden no-highlight`}
+            >
+                <span className="z-10">
+                    { props.lesson.name }<br/>
+                </span>
+                <div 
+                    className={`${finished ? "" : "bg-primary"} 
+                        z-0 w-full absolute bottom-0`} 
+                    style={{
+                        height: `${ waterFillAmount > 150 ? "100" : waterFillAmount }%`
+                    }} 
+                />
+                { showWave && <Lottie 
+                    rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }} 
+                    className={`w-full h-full absolute ${ANIMATION_OFFSET_CLASS}`}
+                    style={{ transform: `translate(0, -${waterFillAmount}px)` }}
+                    animationData={wave} 
+                    goTo={startFrame} 
+                    loop 
+                    play 
+                /> }
+            </div>
+        </Link>
     )
 }
 
