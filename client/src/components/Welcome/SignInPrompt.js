ef12d33351205a51c72213c7d450de5500411867
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-06-02 13:36:50 +0100

    close #183

--- client/src/components/Welcome/SignInPrompt.js
+++ client/src/components/Welcome/SignInPrompt.js
@@ -9,6 +9,15 @@ import SignInButton from "./SignInButton";
 import TranslationsScroller from "./TranslationsScroller";
 
 const TYPING_SEQUENCE_TIME = 3500;
+const DELAY = 1000;
+const TYPING_SEQUENCE = [
+    'learn a new language?',
+    TYPING_SEQUENCE_TIME,
+    DELAY,
+    'recconect with your heritage?',
+    TYPING_SEQUENCE_TIME,
+    DELAY,
+]
 
 function SignInPrompt(props) {
     let navigate = useNavigate();
@@ -27,32 +36,28 @@ function SignInPrompt(props) {
 
     return(
         <animated.div 
-            id="" 
-            className="animate-fade-in z-10 w-4/5 md:w-3/5 pr-14 absolute top-[45%] pl-4"
+            className="animate-fade-in z-0 w-4/5 md:w-3/5 pr-14 
+                absolute top-[45%] pl-4"
             style={spring}
         >
             <animated.div 
-                id="" 
-                className={`text-2xl md:text-3xl tracking-wide font-normal h-20`}
+                className="text-2xl md:text-3xl tracking-wide font-normal h-20"
                 style={spring}
             >
                 <span className="font-bold">Want to </span>  
                 <TypeAnimation
-                    cursor={true}
-                    sequence={[
-                        'learn a new language?',
-                        TYPING_SEQUENCE_TIME,
-                        'recconect with your heritage?',
-                        TYPING_SEQUENCE_TIME,
-                    ]}
                     wrapper="span"
+                    sequence={TYPING_SEQUENCE}
+                    cursor={true}
                     repeat={Infinity}
                 />
             </animated.div>
             <animated.div 
                 id="" 
-                className="text-3xl md:text-4xl tracking-wide mt-6 pb-2 border-b-2 border-white w-48 md:w-56
-                    cursor-pointer transition-all hover:text-white hover:border-primary hover:bg-primary"
+                className="text-3xl md:text-4xl tracking-wide mt-6 pb-2 pl-4 pt-4
+                    border-b-2 border-white w-48 md:w-60
+                    cursor-pointer transition-all z-0
+                    hover:text-white hover:border-primary hover:bg-primary"
                 style={spring}
                 onClick={props.handleClick}
             >
