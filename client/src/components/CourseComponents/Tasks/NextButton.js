84656ebac672511885ad8fe7612f29153f31f791
Balraj Johal <balraj-johal@users.noreply.github.com>
2022-06-14 15:27:31 +0100

    fix #216

--- client/src/components/CourseComponents/Tasks/NextButton.js
+++ client/src/components/CourseComponents/Tasks/NextButton.js
@@ -1,15 +1,28 @@
-import React, {  } from "react";
+import React, { useState } from "react";
 
 function NextButton(props) {
+    let [disabled, setDisabled] = useState(false);
+
+    let handleClick = () => {
+        setDisabled(true);
+        setTimeout(() => {
+            setDisabled(false);
+        }, 600);
+    }
+
     return(
         <div className="w-full flex justify-end h-1/6 pb-4 md:pb-0">
             <button 
+                disabled={disabled}
                 className="md:w-56 w-4/6 h-8 md:h-10
                     cursor-pointer rounded no-highlight
                     bg-primary hover:bg-primary2 hover:drop-shadow-md
                     mb-6 md:mb-4 mt-6 z-20 text-white
                     capitalize transition-all duration-75 md:text-xl" 
-                onClick={()=>{ props.next() }}
+                onClick={()=>{ 
+                    handleClick();
+                    props.next();
+                }}
             >
                 {props.text || "Next"} &gt;
             </button>
