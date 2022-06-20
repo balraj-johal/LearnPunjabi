ac843502332760eeaecf022a6c2d263efde2f4a6
Balraj Johal <balrajsjohal@gmail.com>
2022-06-20 16:36:41 +0100

    add unsuccessful verication feedback

--- client/src/components/VerifyEmail.js
+++ client/src/components/VerifyEmail.js
@@ -9,30 +9,41 @@ import Loader from "./Loader";
 function VerifyEmail(props) {
     let navigate = useNavigate();
     let params = useParams();
+    let [queried, setQueried] = useState(false);
     let [successful, setSuccessful] = useState(false);
 
     useEffect(() => {
         let redirectTimeout;
         if (params.code) {
-            console.log("utting", `/api/v1/users/verify-email/${params.code}`)
             axiosClient.put(`/api/v1/users/verify-email/${params.code}`)
                 .then(result => {
                     setSuccessful(true);
+                    setQueried(true);
                     redirectTimeout = setTimeout(() => {
                         navigate("/account");
                     }, 4000);
                 })
-                .catch(err => { console.log(err); })
+                .catch(err => { 
+                    console.log(err);
+                    setQueried(true);
+                })
         }
 
         return () => { clearTimeout(redirectTimeout) };
     }, [params.code]);
 
     return (
-        <div className="accounts-wrap bg-white shadow-xl rounded" id="verify-email">
-            {successful ? (
+        <div
+            id="verify-email"
+            className="accounts-wrap bg-white shadow-xl rounded"
+        >
+            {queried ? (
                 <div className="flex justify-center items-center text-center h-full p-4">
-                    Email verification successful! Please log in with your details!
+                    {successful ? (
+                        "Email verification successful! Please log in with your details!"
+                    ) : (
+                        "Email verification unsuccessful... Please check your email again."
+                    )}
                 </div>
             ) : (
                 <Loader />
