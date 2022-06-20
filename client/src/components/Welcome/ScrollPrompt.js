bd4636ba5636636ba675b314e549f8687be39c3b
Balraj Johal <balrajsjohal@gmail.com>
2022-06-20 16:16:16 +0100

    add smooth scroll to scroll prompt

--- client/src/components/Welcome/ScrollPrompt.js
+++ client/src/components/Welcome/ScrollPrompt.js
@@ -2,15 +2,26 @@ import React, {  } from "react";
 
 function ScrollPrompt(props) {
     return(
-        <a href={props.scrollTo} className="z-50">
+        <a 
+            href={props.scrollTo} 
+            className="z-50" 
+            onClick={e => {
+                // this is a manual override of the anchor scroll behaviour,
+                // as I could not get smooth scrolling via css to work without
+                // enabling flags in Chrome.
+                e.preventDefault();
+                let scrollTo = document.getElementById(props.scrollTo.substring(1));
+                scrollTo.scrollIntoView({ behavior: "smooth" });
+            }}
+        >
             <div 
                 id="scroll-prompt" 
-                className="flex mr-4 mt-2 cursor-pointer 
+                className="flex items-center mr-4 mt-2 px-4 py-2 absolute 
                     bg-white text-black
-                    md:my-4 lg:my-6 lg:mr-8
-                    absolute
-                    px-4 py-2 items-center" 
+                    md:my-4 
+                    lg:my-6 lg:mr-8" 
                 style={{
+                    // this ensures the element is always above mobile browser UI
                     marginBottom: "calc(env(safe-area-inset-bottom) + 0.5rem)"
                 }}
             >
@@ -23,8 +34,7 @@ function ScrollPrompt(props) {
                 >
                     <path d="M2 2L31 28L60 2" stroke="black" strokeWidth="4"/>
                 </svg>
-                <span className="ml-4 text-md md:text-xl font-bold tracking-wide
-                    ">
+                <span className="ml-4 text-md md:text-xl font-bold tracking-wide">
                     { props.text }
                 </span>
             </div>
