7e4d6778052712fd80a9a464d8968852cdf6a63b
Balraj Johal <balrajsjohal@gmail.com>
2022-06-28 21:41:20 +0100

    improve comments

--- server_routes/courses.js
+++ server_routes/courses.js
@@ -12,36 +12,16 @@ const sanitize = require("mongo-sanitize");
 const router = express.Router();
 
 const { verifyToken } = require("../utilities/authentication");
-const { getFileLink } = require("./s3");
 
 const Course = require("../models/course.model");
 
-// /**
-//  * Get overview of all courses
-//  * @name get/
-//  * @function
-//  * @memberof module:api/courses~coursesRouter
-//  * @param { String } path - route path
-//  * @param { callback } middleware - express middleware
-//  */
-// router.get("/", async (req, res, next) => {
-//     try {
-//         await verifyToken(req);
-//         const overview = await buildOverviewObject();
-//         if (overview) return res.status(200).send({ overview: overview });
-//         return res.status(404).send({ message: "overview data not found..." });
-//     } catch (error) {
-//         next(error);
-//     }
-// })
-
-// /**
-//  * Get latest course data
-//  * @function
-//  * @memberof module:api/courses~coursesRouter
-//  * @param { String } path - route path
-//  * @param { callback } middleware - express middleware
-//  */
+/**
+ * Gets publically available course version
+ * @function
+ * @memberof module:api/courses~coursesRouter
+ * @param { String } path - route path
+ * @param { callback } middleware - express middleware
+ */
 router.get("/", async (req, res) => {
     try {
         await verifyToken(req);
@@ -53,13 +33,15 @@ router.get("/", async (req, res) => {
         return res.status(500).send({error: err});
     }
 })
-// /**
-//  * Get course data by courseID
-//  * @function
-//  * @memberof module:api/courses~coursesRouter
-//  * @param { String } path - route path
-//  * @param { callback } middleware - express middleware
-//  */
+
+/**
+ * Get specific course version
+//  * -- ONLY ADMIN ACCESSIBLE
+ * @function
+ * @memberof module:api/courses~coursesRouter
+ * @param { String } path - route path
+ * @param { callback } middleware - express middleware
+ */
 router.get("/:version", async (req, res, next) => {
     console.log("getting course:", req.params.version)
     try {
@@ -76,25 +58,20 @@ router.get("/:version", async (req, res, next) => {
     }
 })
 
-let getLatestCourse = async () => {
-    const latestCourse = await Course.find().sort({ _id: -1 }).limit(1);
-    if (latestCourse.length < 1) return;
-    return latestCourse[0];
-}
 let getNewVersion = async () => {
     const latestCourse = await Course.find().sort({ _id: -1 }).limit(1);
     if (latestCourse.length < 1) return 1;
     return latestCourse[0].version + 1;
 }
 
-// /**
-//  * Create/update course by courseID
-//  * -- ONLY ADMIN ACCESSIBLE
-//  * @function
-//  * @memberof module:api/courses~coursesRouter
-//  * @param { String } path - route path
-//  * @param { callback } middleware - express middleware
-//  */
+/**
+ * Create new course version
+ * -- ONLY ADMIN ACCESSIBLE
+ * @function
+ * @memberof module:api/courses~coursesRouter
+ * @param { String } path - route path
+ * @param { callback } middleware - express middleware
+ */
 router.post("/", async (req, res) => {
     try {
         const user = await verifyToken(req);
