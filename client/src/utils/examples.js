8ba2f5d766008af8fa1bd7647cb4e6a11e573c59
Balraj Johal <balrajsjohal@gmail.com>
2022-07-13 19:04:56 +0100

    add example lesson

--- client/src/utils/examples.js
+++ client/src/utils/examples.js
@@ -1,167 +1,148 @@
 export const EXAMPLE_LESSON = {
-    "name": "test all",
+    "_id": "62cf041058ca063b27a9be93",
+    "name": "Example",
     "shuffle": false,
-    "showInterstitials": true,
+    "showInterstitials": false,
     "showPercentCorrect": true,
     "tasks": [
         {
             "taskID": "1",
-            "text": "text no audio - Mutations, subscriptions, timers, logging, and other side effects are not allowed inside the main body of a function component (referred to as React’s render phase). Doing so will lead to confusing bugs and inconsistencies in the UI.",
+            "text": "Welcome to Learn Punjabi! In this example lesson we'll teach you a little bit about the Punjabi Alphabet \"Gurmukhi\"!",
             "type": "TextOnly",
             "audioFilename": "",
             "audio": {
                 "name": ""
-            }
+            },
+            "audioTranscript": ""
         },
         {
             "taskID": "2",
-            "text": "text audio",
+            "text": "The first letter is ੳ - \"Ourha\", which makes the \"oo\" sound. Click on the audio button to hear what it sounds like!",
             "type": "TextOnly",
-            "audioFilename": "Logging_Send_For_Review.mp3",
-            "audioLink": "https://balraj-portfolio-bucket.s3.eu-west-2.amazonaws.com/Logging_Send_For_Review.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAUEUMUPDURSWX3FMX%2F20220602%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220602T120818Z&X-Amz-Expires=600000&X-Amz-Signature=17f41e036a29fda368e30228b8431ed74600b363996a19326afb15ddb5b59987&X-Amz-SignedHeaders=host&x-id=GetObject"
+            "audioFilename": "",
+            "audio": {
+                "name": ""
+            },
+            "audioTranscript": ""
         },
         {
             "taskID": "3",
-            "text": "Test 1",
-            "type": "MultipleChoice",
-            "audioFilename": "Error.mp3",
-            "possibleAnswers": [
-                {
-                    "middleText": "1",
-                    "bottomText": ""
-                },
-                {
-                    "middleText": "2",
-                    "bottomText": ""
-                },
-                {
-                    "middleText": "3",
-                    "bottomText": ""
-                },
-                {
-                    "middleText": "4",
-                    "bottomText": ""
-                }
-            ],
-            "correctAnswerIndex": "0",
-            "audioLink": "https://balraj-portfolio-bucket.s3.eu-west-2.amazonaws.com/Error.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAUEUMUPDURSWX3FMX%2F20220602%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220602T120818Z&X-Amz-Expires=600000&X-Amz-Signature=aee43d7f292407483c201b0f615acfb1275e896b76e13d99c1dd429d3193e906&X-Amz-SignedHeaders=host&x-id=GetObject"
-        },
-        {
-            "taskID": "interstitial-0",
-            "text": "",
-            "type": "Interstitial"
+            "text": "The second letter is ਅ - \"Aira\", which makes the \"aa\" sound.",
+            "type": "TextOnly",
+            "audioFilename": "",
+            "audio": {
+                "name": ""
+            },
+            "audioTranscript": ""
         },
         {
             "taskID": "4",
-            "text": "gggggggggg",
+            "text": "And the third letter is ੲ - \"Eerhi\", which makes the \"ee\" sound!",
+            "type": "TextOnly",
+            "audioFilename": "",
+            "audio": {
+                "name": ""
+            },
+            "audioTranscript": ""
+        },
+        {
+            "taskID": "5",
+            "text": "Ready to test your knowledge?",
+            "type": "TextOnly",
+            "audioFilename": "",
+            "audio": {
+                "name": ""
+            },
+            "audioTranscript": ""
+        },
+        {
+            "taskID": "6",
+            "text": "Which letter makes the \"oo\" sound?",
             "type": "MultipleChoice",
             "audioFilename": "",
             "audio": {
                 "name": ""
             },
+            "audioTranscript": "",
             "possibleAnswers": [
                 {
-                    "middleText": "1",
+                    "middleText": "ੳ",
                     "bottomText": ""
                 },
                 {
-                    "middleText": "2",
+                    "middleText": "ਅ",
                     "bottomText": ""
                 },
                 {
-                    "middleText": "3",
-                    "bottomText": ""
-                },
-                {
-                    "middleText": "4",
+                    "middleText": "ੲ",
                     "bottomText": ""
                 }
             ],
             "correctAnswerIndex": "0"
         },
         {
-            "taskID": "5",
-            "text": "1234524",
-            "type": "SpecifiedOrder",
+            "taskID": "7",
+            "text": "Which letter makes the \"ee\" sound?",
+            "type": "MultipleChoice",
             "audioFilename": "",
             "audio": {
                 "name": ""
             },
+            "audioTranscript": "",
             "possibleAnswers": [
                 {
-                    "text": "1",
-                    "id": "a-0"
+                    "middleText": "ੳ",
+                    "bottomText": ""
                 },
                 {
-                    "text": "2",
-                    "id": "a-1"
+                    "middleText": "ਅ",
+                    "bottomText": ""
                 },
                 {
-                    "text": "4",
-                    "id": "a-2"
-                },
-                {
-                    "text": "3",
-                    "id": "a-3"
-                },
-                {
-                    "text": "2",
-                    "id": "a-4"
-                },
-                {
-                    "text": "4",
-                    "id": "a-5"
-                },
-                {
-                    "text": "5",
-                    "id": "a-6"
+                    "middleText": "ੲ",
+                    "bottomText": ""
                 }
             ],
-            "correctAnswer": "1234524"
+            "correctAnswerIndex": "2"
         },
         {
-            "taskID": "6",
-            "text": "qwerty",
-            "type": "SpecifiedOrder",
-            "audioFilename": "Logging_Send_For_Review.mp3",
+            "taskID": "8",
+            "text": "Which letter makes the \"ai\" sound?",
+            "type": "MultipleChoice",
+            "audioFilename": "",
+            "audio": {
+                "name": ""
+            },
+            "audioTranscript": "",
             "possibleAnswers": [
                 {
-                    "text": "y",
-                    "id": "a-0"
+                    "middleText": "ੳ",
+                    "bottomText": ""
                 },
                 {
-                    "text": "t",
-                    "id": "a-1"
+                    "middleText": "ਅ",
+                    "bottomText": ""
                 },
                 {
-                    "text": "r",
-                    "id": "a-2"
-                },
-                {
-                    "text": "e",
-                    "id": "a-3"
-                },
-                {
-                    "text": "w",
-                    "id": "a-4"
-                },
-                {
-                    "text": "q",
-                    "id": "a-5"
+                    "middleText": "ੲ",
+                    "bottomText": ""
                 }
             ],
-            "correctAnswer": "qwerty",
-            "audioLink": "https://balraj-portfolio-bucket.s3.eu-west-2.amazonaws.com/Logging_Send_For_Review.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAUEUMUPDURSWX3FMX%2F20220602%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220602T120818Z&X-Amz-Expires=600000&X-Amz-Signature=17f41e036a29fda368e30228b8431ed74600b363996a19326afb15ddb5b59987&X-Amz-SignedHeaders=host&x-id=GetObject"
+            "correctAnswerIndex": "1"
         },
         {
             "taskID": "end",
-            "text": "Congrats! You've finished the lesson! Now sign up for more :)",
+            "text": "Congrats! You've finished the lesson and learned your first Punjabi letters! If you're interested in continuing sign up for more above! :)",
             "type": "End",
             "showPercentCorrect": true,
             "hideButton": true
         }
     ],
-    "id": "lesson-test all",
-    "requiredCompletions": 75,
+    "id": "lesson-Example",
+    "strId": "lesson-Example",
+    "requiredCompletions": 1,
     "noToSample": 0,
+    "createdAt": "2022-07-13T17:42:40.538Z",
+    "updatedAt": "2022-07-13T17:43:52.784Z",
+    "__v": 1
 }
